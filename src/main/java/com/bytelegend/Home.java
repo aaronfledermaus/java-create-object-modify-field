f786df5f27d16913b9c0ed98f674d1476d89b76c
Laura-bf <laurabf6@gmail.com>
2022-03-29 18:34:55 +0000

    Update at 2022-03-29T18:34:55.006274Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -7,14 +7,9 @@ public class Home {
         System.out.println(newCat("Tom"));
     }
 
-    // Creates a new `Cat` object.
-    // Updates the `name` instance field (aka. member variable) of that object to `name` argument of
-    // the method.
-    // Returns that object.
     public static Cat newCat(String name) {
-        Cat cat = new Cat():
-        cat.name = name;
-
-        return cat.toString();
+    	Cat cat = new Cat();
+    	cat.name = name;
+    	return cat;
     }
 }
