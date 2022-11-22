4d983fda9d9fc5837e8305e82f3204ff98095c82
Lin-kq <2628431588@qq.com>
2022-11-22 11:00:23 +0000

    Update at 2022-11-22T11:00:23.810329Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,7 +1,8 @@
 package com.bytelegend;
 
 import com.bytelegend.pet.Cat;
-
+使之创建一个新的Cat对象， 并将该对象中的name成员变量（又称为实例字段）修改为方法参数name所指定的名字，
+然后返回该对象。
 public class Home {
     public static void main(String[] args) {
         System.out.println(newCat("Tom"));
@@ -11,5 +12,9 @@ public class Home {
     // Updates the `name` instance field (aka. member variable) of that object to `name` argument of
     // the method.
     // Returns that object.
-    public static Cat newCat(String name) {}
+    public static Cat newCat(String name) {
+        Cat cat = new Cat();
+        cat.name = name;
+        return cat;
+    }
 }
