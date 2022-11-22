af9774e4f533e8c0a0133acac6f9e9867d8cd135
Lin-kq <2628431588@qq.com>
2022-11-22 11:01:16 +0000

    Update at 2022-11-22T11:01:16.257325Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -1,8 +1,7 @@
 package com.bytelegend;
 
 import com.bytelegend.pet.Cat;
-使之创建一个新的Cat对象， 并将该对象中的name成员变量（又称为实例字段）修改为方法参数name所指定的名字，
-然后返回该对象。
+
 public class Home {
     public static void main(String[] args) {
         System.out.println(newCat("Tom"));
