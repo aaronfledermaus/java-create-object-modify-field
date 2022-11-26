879c34d5683f4852dd5d9691b6650a187df54bf7
wusuoweiandjr <1047365005@qq.com>
2022-11-26 12:10:58 +0000

    Update at 2022-11-26T12:10:58.454643Z

--- src/main/java/com/bytelegend/Home.java
+++ src/main/java/com/bytelegend/Home.java
@@ -5,11 +5,14 @@ import com.bytelegend.pet.Cat;
 public class Home {
     public static void main(String[] args) {
         System.out.println(newCat("Tom"));
+        System.out.println(newCat("Black"));
     }
 
     // Creates a new `Cat` object.
     // Updates the `name` instance field (aka. member variable) of that object to `name` argument of
     // the method.
     // Returns that object.
-    public static Cat newCat(String name) {}
+    public static Cat newCat(String name) {
+
+    }
 }
